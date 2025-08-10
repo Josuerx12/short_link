6323345336d560d2d90947d0a9fb4a872d230623
josuerx12 <josueaze12@gmail.com>
2025-08-10 18:17:30 -0300

    refact: Adjustments on short link visits app module to authenticate mongoose module with mongo db credentials.

--- short_link_visits/src/app.module.ts
+++ short_link_visits/src/app.module.ts
@@ -6,10 +6,28 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UrlVisitsModule } from './url-visits/url-visits.module';
 import { ShortUrlGatewayModule } from './gateways/short-url/short-url-gateway.module';
 import { TokenGuard } from './guards/token/token.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/short_link_visits'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        console.log(configService.get<string>('DB_URI'));
+
+        return {
+          uri: configService.get<string>('DB_URI'),
+          auth: {
+            username: configService.get<string>('DB_USER'),
+            password: configService.get<string>('DB_PASS'),
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     UrlVisitsModule,
     ShortUrlGatewayModule,
   ],
